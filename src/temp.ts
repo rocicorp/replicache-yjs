c7873c8f8b7b6cc958abe3432244e73c01cf0b12
Erik Arvidsson <erik.arvidsson@gmail.com>
2023-11-09 18:39:48 +0100

    WIP: At least make the monaco sample type safe.

--- src/temp.ts
+++ src/temp.ts
@@ -10,14 +10,24 @@ const ydoc = new Y.Doc();
 const provider = new Provider(reflect, 'monaco', ydoc);
 const type = ydoc.getText('monaco');
 
-const editor = monaco.editor.create(document.getElementById('monaco-editor'), {
-  value: '', // MonacoBinding overwrites this value with the content of type
-  language: 'javascript',
-});
+const editor = monaco.editor.create(
+  must(document.getElementById('monaco-editor')),
+  {
+    value: '', // MonacoBinding overwrites this value with the content of type
+    language: 'javascript',
+  },
+);
 
 const binding = new MonacoBinding(
   type,
-  editor.getModel(),
+  must(editor.getModel()),
   new Set([editor]),
   provider.awareness,
 );
+
+function must<T>(v: T | null | undefined): T {
+  if (v === null || v === undefined) {
+    throw new Error('Expected value to be defined');
+  }
+  return v;
+}
