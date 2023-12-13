24f2664afe5ec89330f3edec2f1c3f3b0da976b3
Cesar Alaestante <cesar@roci.dev>
2023-12-13 13:09:20 -0700

    force-dynamic page

--- examples/tiptap/src/app/page.tsx
+++ examples/tiptap/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { nanoid } from "nanoid";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 function Page() {
   redirect("/r/" + nanoid(6));
 }
--- examples/tiptap/src/app/r/[id]/page.tsx
+++ examples/tiptap/src/app/r/[id]/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { TextEditor } from "@/components/TextEditor";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [, , roomID] = location.pathname.split("/");
+  const [roomID, setRoomID] = useState<string | null>(null);
+  useEffect(() => {
+    const [, , roomID] = location.pathname.split("/");
+    setRoomID(roomID);
+  }, []);
 
   return (
-    <main>
-      <TextEditor roomID={roomID} />
-    </main>
+    roomID && (
+      <main>
+        <TextEditor roomID={roomID} />
+      </main>
+    )
   );
 }
